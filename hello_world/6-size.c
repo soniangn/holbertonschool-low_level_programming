34e41713c62bf0bce1363d3a9ede70895797e408
soniangn <nguyen.sonia@gmail.com>
2022-09-28 05:53:06 -0700

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints sizes
+ * Return: results in 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n Size of an int: %d byte(s)\n Size of a long int: %d byte(s)\n Size of a long long int:%d byte(s) \nSize of a float:%d byte(s)", "1", "4", "4", "8", "4");
+	return (0);
+}

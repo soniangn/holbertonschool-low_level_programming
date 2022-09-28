1f9935e43cb210a69be0dc2ab4ec4db240e32871
soniangn <nguyen.sonia@gmail.com>
2022-09-28 05:12:59 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
+	    //string initialisation
+	char Mystr[] = "/"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 
-	return 0;
+	return (0);
 }

6bf0b8017292553ed118c1a868912ea633cee890
soniangn <nguyen.sonia@gmail.com>
2022-10-03 02:43:50 -0700

    alphabet

---
+++ variables_if_else_while/2-print-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints the alphabet in lowercase
+ */
+int main(void) {
+	char c;
+	while (c != 0) {
+		printf("%d\n", c);
+		c = c+1;
+	}
+}

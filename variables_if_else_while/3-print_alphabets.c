575f962a0405d2c1b0a62446325de0a8539c1fb2
soniangn <nguyen.sonia@gmail.com>
2022-10-03 03:07:47 -0700

    alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints the alphabet in lowercase and in uppercase
+ * Return: 0
+ */
+int main (void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	for (c = 'a'; c <= 'z', c++) 
+	{
+		putchar(c);
+	}
+	for (C = 'A'; C<= 'Z', C++)
+		putchar(C);
+	}
+	putchar("\n");
+	return (0);
+}

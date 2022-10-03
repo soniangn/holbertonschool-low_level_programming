6d92121802cfd291fcbf39718f360b9e83e61486
soniangn <nguyen.sonia@gmail.com>
2022-10-03 10:57:11 -0700

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - entry point
+ * Description: checks for character in lowercase
+ * Return: 0
+ */
+int _islower(int c)
+{
+	int i;
+
+	if (i == 'c')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

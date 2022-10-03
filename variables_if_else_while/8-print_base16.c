7c13a7a228c85215f4a11a26b86b5dc70f33e290
soniangn <nguyen.sonia@gmail.com>
2022-10-03 07:03:01 -0700

    hxa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints all number of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 10; j++)
+			for (k = 'a'; k <= 'f'; k++)
+				putchar(ijk);
+	}
+	putchar('\n');
+	return (0);
+}

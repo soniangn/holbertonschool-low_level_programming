75c726084731920fb544152f4785dc0004b9a1ad
soniangn <nguyen.sonia@gmail.com>
2022-10-03 07:09:36 -0700

    hxa

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,18 +8,11 @@ int main(void)
 {
 	char i;
 	char j;
-	char k;
 	for (i = 0; i <= 10; i++)
 	{
 		putchar(i);
-		for (j = 0; j <= 10; j++)
-		{
+		for (j = 'a'; j <= 'f'; j++)
 			putchar(j);
-		}
-			for (k = 'a'; k <= 'f'; k++)
-			{
-				putchar(k);
-			}
 	}
 	putchar('\n');
 	return (0);

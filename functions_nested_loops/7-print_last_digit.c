61677afe449c231adfed0199c38184f19af12750
soniangn <nguyen.sonia@gmail.com>
2022-10-04 09:32:36 -0700

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ * Description: computes the absolute of an integer
+ * Return: the last digit of the number i
+ */
+int print_last_digit(int i)
+{
+	int i;
+
+	while (i <= 0 || i >= 0)
+		return (i * 10%);
+}

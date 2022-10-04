c51302a4b769884f96d69636a2ac5961cca1a653
soniangn <nguyen.sonia@gmail.com>
2022-10-04 06:09:32 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ * Description: computes the absolute of an integer
+ * Return: 0
+ */
+int _abs(int)
+{
+	int i;
+
+	if (i > 0)
+		_putchar(i);
+	if (i < 0)
+		_putchar('-i');
+}

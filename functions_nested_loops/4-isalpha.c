13e3b2b0f8a33565d82f1ddfe04b13de7b36b59b
soniangn <nguyen.sonia@gmail.com>
2022-10-04 05:06:05 -0700

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - entry point
+ * Description: checks for alphabetic character
+ * parameter c: character to check
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+		return (1);
+	else
+		return (0);
+}

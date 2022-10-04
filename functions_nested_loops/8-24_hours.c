428669d11a8a037b324915bc2f0bc99751c52502
soniangn <nguyen.sonia@gmail.com>
2022-10-04 10:29:26 -0700

    jack bauer

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ * Description: computes the absolute of an integer
+ * Return: the last digit of the number i
+ */
+void jack_bauer(void)
+{
+	int i = '00:00';
+	int j = '23:59';
+
+	while (i < j)
+		_putchar(i);
+		i++;
+}

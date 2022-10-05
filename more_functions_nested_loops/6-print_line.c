590b3a654af4241c141761ab1754c5027014f2dd
soniangn <nguyen.sonia@gmail.com>
2022-10-05 06:56:15 -0700

    print line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,16 @@ void print_line(int n)
 {
 	int i;
 
-	while (i <= n)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
-			i++;
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

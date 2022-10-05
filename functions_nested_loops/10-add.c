30bcc4969348d3dc3a9ca259a89a2b5e82d8e738
soniangn <nguyen.sonia@gmail.com>
2022-10-05 00:49:04 -0700

    a+b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - entry point
+ * @i: first integer
+ * @j: second integer
+ * Description: add two integers and returns the result
+ * Return: results of addition of two integers
+ */
+int add(int i, int j)
+{
+	_putchar(i + j);
+}

4c2fa353c44e060d97e098b0f6e2dd216edac933
soniangn <nguyen.sonia@gmail.com>
2022-10-05 00:30:28 -0700

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,14 @@
 /**
  * print_last_digit - entry point
  * Description: prints the last digit of a number
+ * @i: number
  * Return: the last digit of the number i
  */
 int print_last_digit(int i)
 {
 	if (i >= 0)
 		return (i % 10);
-	else 
-		return (-i % 10);	
+	else
+		return (-i % 10);
 	_putchar ('\n');
 }

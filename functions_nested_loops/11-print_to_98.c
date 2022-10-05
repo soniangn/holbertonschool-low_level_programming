45e720e005f268fe4f3da423bbc54c1974b46f4b
soniangn <nguyen.sonia@gmail.com>
2022-10-05 01:00:24 -0700

    natural

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_to_98 - entry point
+ * @n: natural number
+ * Description: prints all natural numbers from n to 98
+ * Return: natural numbers from n to 98
+ */
+int print_to_98(int n)
+{
+	for (n = 0; n <= 98, n++)
+	{	_putchar(n);
+		_putchar(',');
+		_putchar (' ');
+	}
+}

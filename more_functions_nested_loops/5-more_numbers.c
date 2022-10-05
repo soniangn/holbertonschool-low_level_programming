c38d4e9a6b0e6cd172d018cc746e0dfd514dac70
soniangn <nguyen.sonia@gmail.com>
2022-10-05 06:22:31 -0700

    more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,17 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= '14'; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j > '9')
+			if (j > 9)
 			{
-				_putchar((j / '10') + '0');
+				_putchar((j % 10) + '0');
 			}
 			else
 			{
-				_putchar(j);
+				_putchar((j / 10) + '0';
 			}
 		}
 	}

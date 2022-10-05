2ba08a7294433d40ae2cbf95751a547731fbe554
soniangn <nguyen.sonia@gmail.com>
2022-10-05 04:50:00 -0700

    numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - entry point
+ * Description: prints the numbers, from 0 to 9, followed by a new line
+ * Return: the numbrs printed
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for(i = 0; i <= 9; i++)
+	{	
+		_putchar(i);
+		_putchar(('\n');
+	}
+}

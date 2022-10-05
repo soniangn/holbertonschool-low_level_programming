112fc7f7e572a463fbb44a0d3360ad93cbe824bd
soniangn <nguyen.sonia@gmail.com>
2022-10-05 08:07:29 -0700

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_fizz - entry point
+ * Description: prints numbers from 1 to 100 and Fizz, Buzz, FizzBuzz
+ * Return: numbers
+ */
+void print_fizz(int i)
+{
+	for (i = 1; i <= 100; i++)
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+}

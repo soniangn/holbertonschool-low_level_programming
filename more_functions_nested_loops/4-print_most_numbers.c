4cb6a5bfbfe8fea56ea953587b0157791b7a1bfc
soniangn <nguyen.sonia@gmail.com>
2022-10-05 05:09:56 -0700

    most numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,13 @@
  */
 void print_most_numbers(void)
 {
-	int i = 0;
-	
-	while (i >= '0' && i <= '9')
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
 	{
-		while (i != '2' && i != '4')
+		if (i != '2' && i != '4')
 		{
 			_putchar(i);
-			i++;
 		}
 	}
 	_putchar('\n');

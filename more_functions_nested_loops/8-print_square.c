02fec59c8589b321e90c069dbe022d125a8968ff
soniangn <nguyen.sonia@gmail.com>
2022-10-05 07:56:51 -0700

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,6 +8,7 @@
 void print_square(int size)
 {
 	int i;
+	int j;
 
 	if (size <= 0)
 	{
@@ -17,8 +18,11 @@ void print_square(int size)
 	{
 		for (i = 1; i <= size; i++)
 		{
-			_putchar ('#');
-		}
+			for (j = 1; j <= size; j++)
+			{
+				_putchar ('#');
+			}
 		_putchar('\n');
+		}
 	}
 }

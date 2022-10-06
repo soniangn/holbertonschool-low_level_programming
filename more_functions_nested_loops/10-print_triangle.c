21c3cecaaed050de78c58549d7cb845f6b53e8a1
soniangn <nguyen.sonia@gmail.com>
2022-10-06 13:59:42 -0700

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -16,11 +16,8 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-				_putchar('\n');
-			}
+			_putchar('#');
+			_putchar('\n');
 		}
 	}
 }

f778eaaa882752ffded0c7f098380f2e9c51fef0
soniangn <nguyen.sonia@gmail.com>
2022-10-06 05:31:09 -0700

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,10 +7,18 @@
  * Return: natural numbers from n to 98
  */
 void print_to_98(int n)
-{
-	for (; n <= 98; n++)
-	{	printf("%d", n);
-		printf(",");
-		printf(" ");
-	}
+{ 
+		for (; n <= 98; n++)
+		{	
+			if (n = 98)
+			{
+				printf("%d", n);
+			}
+			else
+			{
+				printf("%d", n);
+				printf(",");
+				printf(" ");
+			}
+		}
 }

633749487df30b379c0298246377e3eda9cad1bb
soniangn <nguyen.sonia@gmail.com>
2022-10-06 03:32:29 -0700

    natural

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,7 +6,7 @@
  * Description: prints all natural numbers from n to 98
  * Return: natural numbers from n to 98
  */
-int print_to_98(int n)
+void print_to_98(int n)
 {
 	for (n = 0; n <= 98; n++)
 	{	printf(%d, n);

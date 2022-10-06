ada576f2f9f50e4dd41bcd115b72c6a7fe42fbc1
soniangn <nguyen.sonia@gmail.com>
2022-10-06 13:13:35 -0700

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -19,9 +19,16 @@ void print_to_98(int n)
 			{
 				for (; n <= 98; n++)
 				{
-					printf("%d", n);
-					printf(",");
-					printf(" ");
+					if (n < 98)
+					{
+						printf("%d", n);
+						printf(",");
+						printf(" ");
+					}
+					else
+					{
+						printf("%d", n);
+					}
 				}
 			}
 		}

913f60ac4e37daaabe4c3dd703c19c59efbfd5fc
soniangn <nguyen.sonia@gmail.com>
2022-10-06 07:42:19 -0700

    table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,19 +13,25 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if ((i*j) < 10)
+			if ((i * j) < 10)
 			{
 				_putchar((i * j) + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 			}
-			else 
+			else
 			{
 				_putchar((i * j) / 10 + '0');
 				_putchar((i * j) % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');

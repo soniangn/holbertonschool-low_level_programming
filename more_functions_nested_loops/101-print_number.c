6bffe76a465beb51259068257e2b9975f1363861
soniangn <nguyen.sonia@gmail.com>
2022-10-07 01:05:54 -0700

    number

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number - entry point
+ * Description: prints an integer
+ * @n: integer
+ * Return: 0
+ */
+void print_number(int n)
+{
+	if (n == 0)
+		_putchar ('0');
+	else
+		_putchar(n);
+}

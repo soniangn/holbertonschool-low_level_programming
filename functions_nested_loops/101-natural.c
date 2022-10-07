3d37474018286530f8dfbfaa19b3889e3674fbfd
soniangn <nguyen.sonia@gmail.com>
2022-10-07 06:15:58 -0700

    natural

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_natural - entry point
+ * Description: prints the sum of multiples of 3 or 5 below 1024 (excluded)
+ * Return: 0
+ */
+int print_natural(void)
+{
+	int i;
+	int j = 0;
+
+	while (i < 1024)
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			j = j + i;
+			i++;
+		}
+	return (j);
+}

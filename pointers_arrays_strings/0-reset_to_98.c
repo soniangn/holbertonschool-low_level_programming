f18472f8a608ea230e9b9f51331037ce05492a9d
soniangn <nguyen.sonia@gmail.com>
2022-10-10 02:03:54 -0700

    reset

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 
 /**
  * reset_to_98 - entry point
- * @*n: pointer to an int
- * Description: takes a pointer to an int as parameter and updates the value it points to to 98
+ * @n: pointer to an int
+ * Description: updates the value it points to to 98
  * Return: 0
  */
 void reset_to_98(int *n)

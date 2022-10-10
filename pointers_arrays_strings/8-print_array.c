ae6619d05bff340f34332bc71ffee01e4e5cfe6c
soniangn <nguyen.sonia@gmail.com>
2022-10-10 06:37:13 -0700

    array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - entry point
+ * @a: an array
+ * @n: number of elements of the array to be printed
+ * Description: prints n elements of an array of integers
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for(i = 0; i < n; i++)
+	{
+		printf("%d", *(a +i));
+		printf(',');
+		printf(' ');
+	}
+}

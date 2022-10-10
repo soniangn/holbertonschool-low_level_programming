5b39186ab98f9ecb37f4d4d7568b41d5fe834644
soniangn <nguyen.sonia@gmail.com>
2022-10-10 06:38:38 -0700

    array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - entry point
@@ -14,7 +15,7 @@ void print_array(int *a, int n)
 	for(i = 0; i < n; i++)
 	{
 		printf("%d", *(a +i));
-		printf(',');
-		printf(' ');
+		printf(",");
+		printf(" ");
 	}
 }

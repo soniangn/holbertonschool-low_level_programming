e61435c4e7d2ae3db306e3c3b27fb164d84e67fe
soniangn <nguyen.sonia@gmail.com>
2022-10-10 10:50:04 -0700

    rev

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+include "main.h"
+
+/**
+ * rev_string - entry point
+ * @s: a string
+ * Description: reverses a string
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+		j++;
+	}
+
+	for (i = j - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+
+}

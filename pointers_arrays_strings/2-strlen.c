6cf1494af9ecf149f609ded2a73aa3b121c0c8d6
soniangn <nguyen.sonia@gmail.com>
2022-10-10 02:56:37 -0700

    length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - entry point
+ * @s: a string
+ * Description: returns the length of a string
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	_putchar(*s);
+}

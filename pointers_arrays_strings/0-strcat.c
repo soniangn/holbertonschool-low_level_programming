d8f1144cb3a4785845182946f4ef0eab8c697802
soniangn <nguyen.sonia@gmail.com>
2022-10-10 11:09:25 -0700

    cat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcat - entry point
+ * @dest: a string
+ * @src: a string
+ * Description: appends the src string to the dest string
+ * Return: pointer to the string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		j++;
+		i++;
+	}
+	
+	for (src[0]; src[i] < src[j], i++)
+	{
+		dest[i] = src[i];
+	}
+
+	_putchar('\n');
+
+}

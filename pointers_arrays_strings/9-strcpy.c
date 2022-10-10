0bb69cc6f099c4932413e67ed159817366c1161a
soniangn <nguyen.sonia@gmail.com>
2022-10-10 07:21:09 -0700

    buffer

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,6 +10,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	dest = *src;
-	return (*dest);
+	int i, n;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return dest;
 }

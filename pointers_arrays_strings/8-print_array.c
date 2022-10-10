1f5daf28b1b6b9f209cd9441dee758f6173312bc
soniangn <nguyen.sonia@gmail.com>
2022-10-10 06:47:36 -0700

    array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		if (i < n)
+		if (i < n - 1)
 			printf("%d", *(a + i));
-		printf(",");
-		printf(" ");
+			printf(",");
+			printf(" ");
+		else
+			printf("%d", *(a + i));
 	}
 }

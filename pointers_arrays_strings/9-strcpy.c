a2e74a922f65db27499529c10ca96cd5a7357d94
soniangn <nguyen.sonia@gmail.com>
2022-10-10 07:01:22 -0700

    buffer

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - entry point
+ * @dest: pointer of a buffer
+ * @src: pointer to a string
+ * Description: copies the string pointed to by src to the pointer dest
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+}

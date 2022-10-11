e6b84dda7d4e5e89bfb01f68500056e4cc98269a
soniangn <nguyen.sonia@gmail.com>
2022-10-11 05:04:30 -0700

    cat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -19,9 +19,9 @@ char *_strcat(char *dest, char *src)
 
 	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
+		dest[i + j] = src[j];
 	}
-	 dest[i] = '\0';
+	 dest[i + j] = '\0';
 	
 	return (dest);
 	_putchar('\n');

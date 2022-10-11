82f30ff1ca675d117576d754323d763f60607d6d
soniangn <nguyen.sonia@gmail.com>
2022-10-11 06:45:24 -0700

    compare

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,7 @@ char _strcmp(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		for (j = 0; s2[j] != '\0'; j++)
-			_putchar(s2[j] - s1[i];	
+			_putchar(s2[j] - s1[i]);	
 	}
 
 	_putchar('\n');

d30bbc5c4ef395acc4b1efc36a372030e7ddd7d7
soniangn <nguyen.sonia@gmail.com>
2022-10-11 11:20:26 -0700

    copy

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ char _strcmp(char *s1, char *s2)
 
 	for (i = 0; *s1 != '\0' && *s2 != '\0'; i++)
 	{
-		if (*s1 < *s2 || *s1 > *s2)
+		if (*s1 != *s2)
 			return (*s1 - *s2);
 		else
 			return ('0');

6708912119954149e072db81bd790b22f46cfff6
soniangn <nguyen.sonia@gmail.com>
2022-10-11 11:39:48 -0700

    array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - entry point
+ * @a: pointer to an array of int
+ * @n: number of elements of the array
+ * Description: reverses the content of an array of int
+ * Return: 0
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = n - 1; i >= (n / 2); i--)
+	{
+		j =	a[i];
+		a[j] = a[n - i - 1];
+		a[n - i - 1] = j;
+	}
+}

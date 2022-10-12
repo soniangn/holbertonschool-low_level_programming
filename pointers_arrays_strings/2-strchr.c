ed6dac60aee1a394ca8598ec042ea31b0129e0c2
soniangn <nguyen.sonia@gmail.com>
2022-10-12 09:10:10 -0700

    character

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strchr - entry point
+ * @s: a string
+ * @c: a character to find
+ * Description: locates a character in a string
+ * Return: pointer to the first occurence of
+ *        the character c in the string, or NULL
+ *        if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (*s++)
+	{
+		if (*s == 'c')
+			return (*s);
+		else
+			return (NULL);
+	}
+}	

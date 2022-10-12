4fb9f1bb4805341e769a9838b8b1af30d1b4b3fd
soniangn <nguyen.sonia@gmail.com>
2022-10-12 04:17:26 -0700

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,10 +10,10 @@ char *leet(char *s)
 {
 	int i;
 	int n;
-	char a[5] = {'a', 'e', 'o', 't', 'l'}; 
+	char a[5] = {'a', 'e', 'o', 't', 'l'};
 	char b[5] = {'A', 'E', 'O', 'T', 'L'};
 	char c[5] = {'4', '3', '0', '7', '1'};
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (n = 0; n < 5; n++)

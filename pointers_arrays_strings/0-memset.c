78ef46182b41701cf9353f3e1d76b2ef5b9f532e
soniangn <nguyen.sonia@gmail.com>
2022-10-12 06:37:06 -0700

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *_memset - entry point
+ * @s: memory area
+ * @b: constant byte
+ * @n: first n bytes of the memory area
+ * Description: fills memory with a constant byte
+ * Return: pointer to the memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+}

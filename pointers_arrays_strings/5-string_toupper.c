cb3342522fdef7af24cf3f01b0edae1e67f97dc3
soniangn <nguyen.sonia@gmail.com>
2022-10-12 01:12:20 -0700

    uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *string_toupper - entry point
+ * Description: changes alle lowercase letters of a string
+ *				to uppercase
+ * Return: uppercase string
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; *[i] != '\0'; i++)
+	{
+		if (*[i] >= 'a' && *[i] <= 'a')
+			*[i] -= 32;
+	}
+	return (*)
+}

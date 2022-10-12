0052051ca0a48db47e81492b70d1b366459840d2
soniangn <nguyen.sonia@gmail.com>
2022-10-12 01:57:09 -0700

    capitalized

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,15 +9,17 @@
 char *cap_string(char *s)
 {
 	int i = 0;
+	int n = 0;
 	int a[10] = {33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 	
-	if (*s <= a[0] || *s >= a[0])
+	while (s[i] != a[n])
 	{
 		for (; s[i + 2] != '\0'; i++)
 		{
 			if (s[i] >= 'a' && s[i] <= 'z')
 				s[i] -= 32;
 		}
+		n++;
 	}
 	return (s);
 }

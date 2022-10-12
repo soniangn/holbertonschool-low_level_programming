ba43e6dabf86fa475919021e7b2a44d0e4ae27f1
soniangn <nguyen.sonia@gmail.com>
2022-10-12 04:02:33 -0700

    leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *leet - entry point
+ * @s: string to be encoded
+ * Description: encoded a string into 1337
+ * Return: string encoded
+ */
+char *leet(char *s)
+{
+	int i;
+	int n;
+	char a[5] = {'a', 'e', 'o', 't', 'l'}; 
+	char b[5] = {'A', 'E', 'O', 'T', 'L'};
+	char c[5] = {'4', '3', '0', '7', '1'};
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; s[i] == a[n] || s[i] == b[n]; n++)
+		{
+			s[i] == s[n];
+		}
+	}
+	return (s);
+}

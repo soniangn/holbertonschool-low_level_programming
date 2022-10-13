eeb066852ceb1b5e70413dcb946cf08a59ea6fda
soniangn <nguyen.sonia@gmail.com>
2022-10-13 13:11:43 -0700

    chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * print_chessboard - entry point
+ * @a: array of chessboard
+ * Description: prints the chessboard
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j; 
+
+	for (i = 0; i <= 8; i++)
+		for (j = 0; j  <= 8; j++)
+			_putchar(a[i][j];
+}

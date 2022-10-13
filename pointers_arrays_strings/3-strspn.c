e29c47147a370868bc397de7a1f83ab8245d9e6c
soniangn <nguyen.sonia@gmail.com>
2022-10-13 05:46:04 -0700

    substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strspn - entry point
+ * @s: a string
+ * @accept: a string
+ * Description: gets the length of a prefix substring
+ * Return: the number of bytes in the initial segment of
+ *         s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	
+	for (i = 0; accept[i] >= 0; i++)
+	{
+		s[n] = accept[i];
+		n++;
+	}
+	return (n + 1);
+}

8db128f480d959d45f3f54ffa21eba6e160d2f19
soniangn <nguyen.sonia@gmail.com>
2022-10-13 11:36:24 -0700

    string

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,17 +10,18 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
-	unsigned int n = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j]; j++)
 		{
-			if (accept[j] == s[i])
-				n++;
-			else
+			if (s[i] == accept[j])
+			{
 				break;
+			}
 		}
+		if (accept[j] == '\0')
+			break;
 	}
-	return (n);
+	return (i);
 }

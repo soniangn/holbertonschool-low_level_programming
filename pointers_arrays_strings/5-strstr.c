3c1217da502a0ae38f420ef2751f28d5ec3df80a
soniangn <nguyen.sonia@gmail.com>
2022-10-16 04:51:41 -0700

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -10,17 +10,15 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
+	int i = 0;
 
 	while (*haystack)
 	{
-		for (i = 0; needle[i]; i++)
+		while (needle[i] == haystack[i])
 		{
-			while (needle[i] == *haystack)
-			{
-				if (needle[i + 1] == '\0')
-					return (haystack);
-			}
+			if (needle[i + 1] == '\0')
+				return (haystack);
+			i++;
 		}
 		haystack++;
 	}

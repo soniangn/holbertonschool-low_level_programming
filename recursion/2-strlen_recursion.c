68665d697cc9fa2d5b6fc928304ddcc2a09e7bcf
soniangn <nguyen.sonia@gmail.com>
2022-10-17 02:39:46 -0700

    length

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -4,6 +4,7 @@
  * _strlen_recursion - entry point
  * @s: string
  * Description: returns the length of a string
+ * Return: the length of a string
  */
 
 int _strlen_recursion(char *s)
@@ -12,8 +13,12 @@ int _strlen_recursion(char *s)
 
 	if (*s != '\0')
 	{
+		return (0);
+	}
+	else
+	{
 		_strlen_recursion(s + 1);
 		i++;
 	}
-	return (i);
+	return (i + 1);
 }

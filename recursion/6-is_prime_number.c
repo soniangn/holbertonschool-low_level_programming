acfbde86cddc0864323c9c660840335b3a0cf60c
soniangn <nguyen.sonia@gmail.com>
2022-10-17 06:30:32 -0700

    prime

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,9 +2,11 @@
 
 /**
  * is_prime_number - entry point
- * @n: given number
+ * check_prime - checks if the given number is prime
+ * @i: divider
+ * @n: given number to check
  * Description: indicates if n is a prime number
- * Return: 1 is the input integer is a prime number, 
+ * Return: 1 is the input integer is a prime number,
  *          otherwise return 0
  */
 
@@ -13,13 +15,13 @@ int check_prime(int i, int n)
 	if (n <= 0)
 		return (0);
 	if (n == i)
-		return (0); 
+		return (0);
 	if (n % 2 == 0)
 		return (0);
-	if (i <= n/2)
+	if (i <= n / 2)
 		return (1);
 	else
-		return(check_prime(i + 1, n));
+		return (check_prime(i + 1, n));
 }
 
 int is_prime_number(int n)

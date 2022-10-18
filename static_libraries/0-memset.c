cc731a579474b60bfaebc7d3afcbd7519856d0d5
soniangn <nguyen.sonia@gmail.com>
2022-10-18 00:58:43 -0700

    memset

--- static_libraries/0-memset.c
+++ static_libraries/0-memset.c
@@ -1,3 +1,18 @@
 #include "main.h"
 
-char *_memset(char *s, char b, unsigned int n);
+/**
+ *  * *_memset - entry point
+ *   * @s: memory area
+ *    * @b: constant byte
+ *     * @n: first n bytes of the memory area
+ *      * Description: fills memory with a constant byte
+ *       * Return: pointer to the memory area
+ *        */
+char *_memset(char *s, char b, unsigned int n)
+{
+		unsigned int i;
+
+			for (i = 0; i < n; i++)
+						s[i] = b;
+				return (s);
+}

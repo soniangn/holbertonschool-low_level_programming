da5b03d3b3c3a71ce35d7a7a0c40affdfa84effd
soniangn <nguyen.sonia@gmail.com>
2022-10-18 01:53:17 -0700

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}

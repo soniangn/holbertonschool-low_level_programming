64e0bdc5fb630d92c53ae4723f103e814c9421c6
soniangn <nguyen.sonia@gmail.com>
2022-10-18 02:17:46 -0700

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of string
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

429bab84da12c3d4d0d8ac36f50821845c34d001
soniangn <nguyen.sonia@gmail.com>
2022-10-18 05:26:10 -0700

    add task

---
+++ argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, n = 0;
+
+	if (argc == 0)
+	{
+		printf("%s\n", "0");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (! atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		else
+		{
+			n += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", n);
+	return (0);
+}

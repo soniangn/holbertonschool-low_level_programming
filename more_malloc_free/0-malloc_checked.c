47c27f4f6acf78e969a412bc9838e281b9162023
soniangn <nguyen.sonia@gmail.com>
2022-10-25 03:14:47 -0700

    task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *malloc_checked - entry point
+ * @b: size
+ * Description: allocates memory using malloc
+ * Return: 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *memory;
+
+	memory = malloc(b);
+
+	if (memory == NULL)
+	{
+		free(memory);
+		exit (98);
+	}
+	return (memory);
+	return (0);
+}

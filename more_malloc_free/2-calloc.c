e1bd5b8529fe02fcedd2b227443c326c6e37d95d
soniangn <nguyen.sonia@gmail.com>
2022-10-25 06:26:38 -0700

    task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * *_calloc - entry point
+ * @nmemb: elements of the array
+ * @size: elements bytes
+ * Description: allocates memory for an array of nmemb elements
+ *	Return: a pointer to the allocated memory
+ *			Return NULL on failure
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *_cal;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	_cal = malloc(nmemb * size);
+	
+	if (_cal == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		_cal[i] = 0;
+	
+	return (_cal);
+	return (0);
+}

52e972251f8779eccffe05a7ae79ade2b0478426
soniangn <nguyen.sonia@gmail.com>
2022-10-26 02:07:58 -0700

    task 2

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - entry point
+ * Description: prints the name of the file is was
+ *				compiled from
+ * Return: 0
+ */
+
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);

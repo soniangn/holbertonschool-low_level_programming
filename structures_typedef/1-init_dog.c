ceb86a8a6434bcd52b95e36da8b6a0ff3eeaed0c
soniangn <nguyen.sonia@gmail.com>
2022-10-31 02:42:09 -0700

    task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - entry point
+ * Description: initialization of a variable of type struct dog
+ * @d: pointer to the structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: 0
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

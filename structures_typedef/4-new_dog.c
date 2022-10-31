b283cfa0170636256d4e27616e8e8ba8f1432cad
soniangn <nguyen.sonia@gmail.com>
2022-10-31 03:49:48 -0700

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,9 +14,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	char copy_name = name;
-	char copy_owner = owner;
-
+	
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 	{
@@ -27,5 +25,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->name = name;	
 	d->age = age;
 	d->owner = owner;
+	memcpy(name, owner);
 	return (d);
 }

30b945fc92408c8bf6c7ad753e8c498e4eca1753
soniangn <nguyen.sonia@gmail.com>
2022-10-31 03:47:53 -0700

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,18 +14,18 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char copy_name = name;
+	char copy_owner = owner;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
+	{
 		return (NULL);
 		free (d);
+	}
 
-	d->name = name;
-	n = name;
-	
+	d->name = name;	
 	d->age = age;
-	
 	d->owner = owner;
-	o = owner;
 	return (d);
 }

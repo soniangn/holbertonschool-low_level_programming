2166f016a99445563bc83abdc0de68583ce8b7e6
soniangn <nguyen.sonia@gmail.com>
2022-10-31 11:46:00 -0700

    task 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -5,10 +5,13 @@
  * print_name - entry point
  * Description: prints a name
  * @name: name to print
+ * @f: pointer to a function
  * Return: 0
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
 	f(name);
 }

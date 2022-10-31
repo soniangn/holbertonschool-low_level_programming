f3ef33d65edb433ea062eff9ac200944ea835716
soniangn <nguyen.sonia@gmail.com>
2022-10-31 06:27:30 -0700

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,6 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 	char *copy_name;
 	char *copy_owner;
+	int i;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
@@ -24,8 +25,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d);
 	}
 
-	copy_name = malloc(sizeof(name));
-	copy_owner = malloc(sizeof(owner));
+	length_name = strlen(name);
+	length_owner = strlen(owner);
+
+	copy_name = malloc(sizeof(char) * length_name);
+	copy_owner = malloc(sizeof(char) * length_owner);
+
+	for (i = 0; name[i] != '\0'; i++)
+		copy_name[i] = name[i];
+
+	for (i = 0; owner[i] != '\0'; i++)
+		 copy_owner[i] = owner[i];
 
 	d->name = name;
 	d->age = age;

75073e0054560ce74e4885f3923f1ed23eb7a245
soniangn <nguyen.sonia@gmail.com>
2022-11-02 03:20:05 -0700

    task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -20,11 +20,12 @@ int (*get_op_func(char *s))(int, int)
         {"%", op_mod},
         {NULL, NULL}
     };
+
     int i = 0;
 
 	while (ops[i].op != NULL)
 	{
-		if (*s == *ops[i].op)
+		if (*ops[i].op == *s)
 			return (ops[i].f);
 		i++;
 	}
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -7,34 +7,30 @@
  * Description: prints the result of the operation
  * @argc: number of argument
  * @argv: array of arguments
- * Return: 0
+ * Return: 0. Exit 98 if argc wrong, exit 99 if wrong
+ *			operator, exit 100 if wrong operation
  */
 
 int main(int argc, char *argv[])
 {
-
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[3]);
-	int result = (get_op_func(argv[2]))(num1, num2);
+	int operator = *argv[2];
+	int (*result)(int, int);
+	result = (get_op_func(argv[2]));
 
-	if (argc != '4')
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (get_op_func(argv[2]) == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == '0')
+	if ((operator == '/' || operator == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", result);
+	printf("%d\n", result(num1, num2));
 	return (0);
 }

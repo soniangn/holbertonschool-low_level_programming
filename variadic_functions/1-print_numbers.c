56ddaec45c18464a6d4ea90fa42cea625500b682
soniangn <nguyen.sonia@gmail.com>
2022-11-02 05:29:07 -0700

    task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - entry point
+ * Description: prints numbers
+ * @n: unsigned integer
+ * Return: 0
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int nb;
+	unsigned int i = 0;
+
+	va_list numbers;
+
+	if (separator == NULL)
+		return;
+
+	va_start(numbers, n);
+
+	for (; i < n; i++)
+	{
+		nb = va_arg(numbers, const unsigned int);
+		printf("%d", nb);
+
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+
+	va_end(numbers);
+	}

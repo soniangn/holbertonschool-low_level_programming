68908e4f8d55d80c594fc942dcf741d84a84c257
soniangn <nguyen.sonia@gmail.com>
2022-11-07 01:46:12 -0800

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,32 +4,36 @@
 
 /**
  * print_char - prints a character
+ * Description: prints a character
  * @arg: a list of arguments to be printed
  * Return: 0
  */
 
 void print_char(va_list arg)
 {
-	char character;
-	character = va_arg(arg, int);
-	printf("%c", character);
+	char cht;
+	cht = va_arg(arg, int);
+	printf("%c", cht);
 }
 
  /**
   * print_integer - prints an integer
+  * Description: prints an integer
   * @arg: a list of arguments to be printed
   * Return: 0
   */
 
 void print_integer(va_list arg)
 {
-    int number;
-    number = va_arg(arg, int);
-    printf("%d", number);
+	int number;
+
+	number = va_arg(arg, int);
+	printf("%d", number);
 }
 
  /**
   * print_float - prints a float
+  * Description: prints a float
   * @arg: a list of arguments to be printed
   * Return: 0
   */
@@ -37,12 +41,14 @@ void print_integer(va_list arg)
 void print_float(va_list arg)
 {
 	float decimal;
+
 	decimal = va_arg(arg, double);
 	printf("%f", decimal);
 }
 
  /**
   * print_string - prints a string
+  * Description: prints a string
   * @arg: a list of arguments to be printed
   * Return: 0
   */
@@ -50,14 +56,10 @@ void print_float(va_list arg)
 void print_string(va_list arg)
 {
 	char *word;
+
 	word = va_arg(arg, char *);
 
-	if (word == NULL)
-	{
-		printf("(nil)");
-	}
-
-	printf("%s", word);
+	word == NULL ? printf("%p", word) : printf("%s", word);
 }
 
  /**
@@ -81,19 +83,24 @@ void print_all(const char * const format, ...)
 		{"s", print_string},
 	};
 
-	va_start (args, format);
+	va_start(args, format);
 
 	while (format[i])
 	{
 		while (n < 4 && (format[i] != *(func[n].character)))
-				n++;
+		{
+			n++;
+		}
 
 		if (n < 4 && (format[i] == *(func[n].character)))
-		{	
+		{
 			func[n].print_type(args);
-			printf("%s", separator);
-		}
+
+			if (format[i + 1] != '\0')
+				printf("%s", separator);
+		}	
 		i++;
+		n = 0;
 	}
 	printf("\n");
 	va_end(args);

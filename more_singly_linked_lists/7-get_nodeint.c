17be6bae5ca773a948c1d5f69a91ce9eba93f3b7
soniangn <nguyen.sonia@gmail.com>
2022-11-14 14:35:04 -0800

    removed pointer p

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -16,8 +16,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	p = head;
 
-	for (; i <= index; i++)
+	for (; i < index; i++)
 	{
+		if (p == NULL)
+			return (NULL);
+
 		p = p->next;
 	}
 

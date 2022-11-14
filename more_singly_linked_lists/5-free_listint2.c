ba31234023b1d4f759b4212df5404ca71709caf3
soniangn <nguyen.sonia@gmail.com>
2022-11-14 08:40:28 -0800

    created function

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - entry point
+ * Description: frees a listint_t list
+ * @head: pointer to the head node
+ * Return: 0
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *p;
+
+	while (*head != NULL)
+	{
+		p = *head;
+		*head = (*head)->next;
+		free(p);
+	}
+	*head = NULL;
+}

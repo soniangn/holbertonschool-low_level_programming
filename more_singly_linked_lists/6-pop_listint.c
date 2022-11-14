13913bb2ec32ac1549c90055c27bbfbbf3de27d7
soniangn <nguyen.sonia@gmail.com>
2022-11-14 11:19:54 -0800

    created function

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - entry point
+ * Description: deletes the head node of a listint_t list
+ * @head: pointer to the pointer to the head node
+ * Return: the head node's data (n)
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *p;
+
+	p = *head;
+	*head = (*head)->next;
+	free(p);
+
+	return ((*head)->n);
+}

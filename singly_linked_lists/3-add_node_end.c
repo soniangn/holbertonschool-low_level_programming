8d04af30aa7e507747761f92ccf094075e3cf350
soniangn <nguyen.sonia@gmail.com>
2022-11-14 06:08:57 -0800

    updates task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -14,23 +14,16 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *last_node;
-	char *copy;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
+		free(new_node);
 		return (NULL);
 	}
 
-	copy = strdup(str);
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-
-	new_node->str = copy;
-	new_node->len = strlen(copy);
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
 	new_node->next = NULL;
 
 	if (*head == NULL)
@@ -46,6 +39,4 @@ list_t *add_node_end(list_t **head, const char *str)
 		last_node->next = new_node;
 	}
 	return (new_node);
-	free(copy);
-	free(new_node);
 }

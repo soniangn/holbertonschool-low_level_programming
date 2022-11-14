af6133363dad923b4719692d8f4ed69f4adcf250
soniangn <nguyen.sonia@gmail.com>
2022-11-14 06:02:22 -0800

    updates task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -25,12 +25,6 @@ list_t *add_node(list_t **head, const char *str)
 
 	copy = strdup(str);
 
-	if (copy == NULL)
-	{
-		return (NULL);
-		free(copy);
-	}
-
 	new_node->str = copy;
 	new_node->len = strlen(copy);
 	new_node->next = *head;

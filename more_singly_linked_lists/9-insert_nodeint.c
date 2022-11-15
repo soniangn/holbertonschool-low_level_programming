a08660ed1a1c0e99bcba270a8238a526f4833ce6
soniangn <nguyen.sonia@gmail.com>
2022-11-15 02:52:56 -0800

    rectified the size call

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -3,18 +3,16 @@
 #include <stddef.h>
 
 /**
- * insert_nodeint_at_index - entry point
- * Description: adds a new node at a given position
- * @head: pointer to the pointer to the head node
- * @idx: the index of the list where the new node should be added
- * @n: the element to add
- * Return: the address of the new element, or NULL if failed
+ * get_size - entry point
+ * Description: finds the length of the listint_t list
+ * @head: pointer to the head node
+ * Return: the size of the list
  */
 
-int get_size(listint_t *head)
+unsigned int get_size(listint_t *head)
 {
-	int size = 0;
-	
+	unsigned int size = 0;
+
 	while (head != NULL)
 	{
 		head = head->next;
@@ -23,11 +21,23 @@ int get_size(listint_t *head)
 	return (size);
 }
 
+/**
+ * insert_nodeint_at_index - entry point
+ * Description: adds a new node at a given position
+ * @head: pointer to the pointer to the head node
+ * @idx: the index of the list where the new node should be added
+ * @n: the element to add
+ * Return: the address of the new element, or NULL if failed
+ */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *previous_node;
 	unsigned int i = 0;
-	int size = get_size(*head);
+	unsigned int size = get_size(*head);
+
+	if (idx > size)
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 
@@ -39,9 +49,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 	new_node->next = NULL;
-
-	if (i < size || i > size)
-		return (NULL);
 
 	previous_node = *head;
 

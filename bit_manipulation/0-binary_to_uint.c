f50d345ad83082491c5dac2bc690f5bdd183e14b
soniangn <nguyen.sonia@gmail.com>
2022-11-16 07:05:48 -0800

    changed unsigned int i to int i

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int uint = 0;
 	unsigned int base = 1;
-	unsigned int i = 0;
+	int i = 0;
 
 	if (b == NULL)
 		return (0);
@@ -21,17 +21,17 @@ unsigned int binary_to_uint(const char *b)
 	while (b[i] != '\0')
 		i++;
 
-	i -= 1;
+	i--;
 
-	while (i)
+	while (i >= 0)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
+		if (b[i] == '1')
+			uint = uint + base;
+
+		if ((b[i] != '1') && (b[i] != '0'))
 			return (0);
 
-		if (b[i] == '1')
-			uint += base;
-		
-		base *= 2;
+		base = base * 2;
 		i--;
 	}
 

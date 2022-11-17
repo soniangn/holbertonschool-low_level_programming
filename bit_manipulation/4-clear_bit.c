6fc7661c09905e3c35e7d2e7ccdd9cb11c43e4b3
soniangn <nguyen.sonia@gmail.com>
2022-11-17 02:53:10 -0800

    created function

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * clear_bit - entry point
+ * Description: sets the value of a bit to 0 at a given index
+ * @n: pointer to number converted in binary
+ * @index: given index, starting from 0 of the bit wanted to set
+ * Return: 1 if worked or -1 if error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask;
+
+	mask = ~(1 << index);
+
+	*n &= mask;
+
+	return (1);
+}

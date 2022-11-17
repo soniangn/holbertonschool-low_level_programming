9fa4430cfba86ee54ff2630c7912c93fff520579
soniangn <nguyen.sonia@gmail.com>
2022-11-17 02:40:50 -0800

    corrected function if index is out

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -4,16 +4,18 @@
 /**
  * set_bit - entry point
  * Description: sets the value of a bit to 1 at a given index
- * @n: pointer to number
+ * @n: pointer to number converted in binary
  * @index: given index, starting from 0 of the bit wanted to set
  * Return: 1 if worked or -1 if error
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int mask = 1 << index;
+	unsigned int mask;
 
 	if (index > *n)
 		return (-1);
+
+	mask = 1 << index;
 
 	*n |= mask;
 

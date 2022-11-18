7de75e5766095b333b76c92e7fc661a43c7b5ab2
soniangn <nguyen.sonia@gmail.com>
2022-11-18 01:45:36 -0800

    created function

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ * Description: prints a specific string
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (1);
+}

a29a77e63c71b4368aeabd3f555e33a6f6374181
soniangn <nguyen.sonia@gmail.com>
2022-11-18 02:10:25 -0800

    created function

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - entry point
+ * @s: double pointer to a string
+ * @to: pointer to a char
+ * Description: sets the value of a pointer to a char
+ * Return: 0
+ */
+void set_string(char **s, *to)
+{
+	*s = to;
+}

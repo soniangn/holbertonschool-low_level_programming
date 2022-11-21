3c693b0f880b945d8401acd2c3a8a8564706d6c6
soniangn <nguyen.sonia@gmail.com>
2022-11-21 06:53:46 -0800

    modified _strlen

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -40,7 +40,7 @@ int create_file(const char *filename, char *text_content)
 	if (new_file == -1)
 		return (-1);
 
-	if (text_content)
+	if (text_content != NULL)
 	{
 		write_file = write(new_file, text_content, length);
 		if (write_file == -1)

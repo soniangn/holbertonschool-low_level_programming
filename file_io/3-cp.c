4c1b958e497d105c92d7bb05a43c460081deeb0b
soniangn <nguyen.sonia@gmail.com>
2022-11-21 12:35:31 -0800

    modified function cases

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,9 +17,10 @@ int main(int argc, char *argv[])
 	/* Checks number of arguments passed */
 	if (argc != 3)
 	{
-		exit(97);
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		exit(97);
 	}
+	
 
 	/* calls the cp function */
 	copy_file(argv[1], argv[2]);
@@ -42,8 +43,8 @@ int copy_file(const char *file_from, const char *file_to)
 	/* Handles absence of file_from */
 	if (file_from == NULL)
 	{
-		exit(98);
 		dprintf(STDERR_FILENO, "Error: Can't read from %s", file_from);
+		exit(98);
 	}
 
 	/* Creates or truncates file_to */
@@ -52,8 +53,8 @@ int copy_file(const char *file_from, const char *file_to)
 	/* Handles failure of file_to creation */
 	if (open_file_to == -1)
 	{
-		exit(99);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s", file_to);
+		exit(99);
 	}
 
 	/* Creates a buffer to read 1024 at a time */
@@ -66,8 +67,8 @@ int copy_file(const char *file_from, const char *file_to)
 	read_file_from = read(open_file_from, buf, 1024);
 	if (read_file_from == -1)
 	{
-		exit(98);
 		dprintf(STDERR_FILENO, "Error: Can't read from %s", file_from);
+		exit(98);
 	}
 
 	/* Copy file1 to file2 */
@@ -76,8 +77,8 @@ int copy_file(const char *file_from, const char *file_to)
 		copied_file = write(open_file_to, buf, 1024);
 		if (copied_file == -1)
 		{
-			exit(99);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s", file_to);
+			exit(99);
 		}
 	}
 

9730a20e94381ae9f3972d6759d0bbf7d8dd7f57
soniangn <nguyen.sonia@gmail.com>
2022-11-21 08:03:49 -0800

    modified type

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	temp = malloc(sizeof(char) * letters + 1);
+	temp = malloc(sizeof(size_t) * letters + 1);
 	if (temp == NULL)
 	{
 		free(temp);

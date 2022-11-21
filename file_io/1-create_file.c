bb7c70908e6241c9773cd927311835c505e3cd94
soniangn <nguyen.sonia@gmail.com>
2022-11-21 06:22:13 -0800

    created function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,13 +23,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 		creat(filename, 0600);
-
-	temp = malloc(sizeof(char) * length + 1);
-	if (temp == NULL)
-	{
-		free(temp);
-		return (0);
-	}
+		write(filename, text_content, length);
 
 	new_file = creat(filename, 0600);
 
@@ -37,7 +31,6 @@ int create_file(const char *filename, char *text_content)
 	if (file_content == -1)
 		return (-1);
 
-	return (1);
 	close(new_file);
-	free(temp);
+	return (1);
 }

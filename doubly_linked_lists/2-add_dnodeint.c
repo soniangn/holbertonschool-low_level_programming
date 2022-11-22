b7ba6ed421ecbab4a7a4bae7d1dbca14c5a18536
soniangn <nguyen.sonia@gmail.com>
2022-11-22 01:53:44 -0800

    head->previous point to new_node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,6 +17,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	/* initializes value of new node n */
 	new_node->n = n;
@@ -25,9 +30,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->next = NULL;
 	new_node->next = *head;
-	*head = new_node;
 	new_node->prev = NULL;
-	
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	(*head) = new_node;
+
 	/* return new node element n */
 	return (new_node);
 }

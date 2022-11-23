7e77ab268b81203521357d511a39a4093657198e
soniangn <nguyen.sonia@gmail.com>
2022-11-23 10:10:47 -0800

    created function

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_get - entry point
+ * Description: retrieves a value associated with a key
+ * @ht: pointeur to the hash table
+ * @key: the key looked for
+ * Return: the value associated with the element or NULL if not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t *item = NULL;
+	char *value;
+
+	if (key == NULL || ht == NULL)
+		return (NULL);
+
+	/* Application of the djb2 function to the element to get the index */
+	index = key_index((unsigned char *)key, ht->size);
+
+	/* Traversal of the array to find the index */
+	item = ht->array[index];
+	while (item)
+	{
+		if (item->key == key)
+			value = item->value;
+		item = item->next;
+	}
+	free(item);
+	return (value);
+}
+

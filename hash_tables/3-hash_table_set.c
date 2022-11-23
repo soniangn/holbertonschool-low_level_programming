be6d75b5ffcb55c30bf64b598173e1daa3bf2f05
soniangn <nguyen.sonia@gmail.com>
2022-11-23 09:59:28 -0800

    set copies to 0

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -34,6 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(temp->value);
 			temp->value = value_copy;
+			free(temp->value);
 			free(temp);
 			return (1);
 		}

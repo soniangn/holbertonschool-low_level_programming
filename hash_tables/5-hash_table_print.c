7d764a516e5b255968eede75426056bc4ba42968
soniangn <nguyen.sonia@gmail.com>
2022-11-25 03:43:57 -0800

    modfied condition to not print comma at the end

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -24,7 +24,7 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				if (flag == 1)
 					printf(", ");
-				
+			
 				flag = 1;
 				printf("'%s': '%s'", item->key, item->value);
 				item = item->next;

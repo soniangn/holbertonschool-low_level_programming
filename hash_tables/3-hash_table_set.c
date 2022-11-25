26378a18aa1a9651e3537ba866824b237a93676a
soniangn <nguyen.sonia@gmail.com>
2022-11-25 02:37:39 -0800

    modified strcmp(temp->key, key) == 0

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -32,7 +32,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(temp->value);
 			temp->value = value_copy;
-			free(temp);
 			return (1);
 		}
 		temp = temp->next;

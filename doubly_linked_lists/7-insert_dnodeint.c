e955428b28621fa92011a3d9fd09af3384424772
soniangn <nguyen.sonia@gmail.com>
2022-11-25 01:32:02 -0800

    handles condition if the list is null

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -50,7 +50,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = NULL;
 	new_node->prev = NULL;
 
-	if (idx == 0)
+	if ((idx == 0) || (*h == NULL && idx == 0))
 	{
 		new_node->next = *h;
 		(*h)->prev = new_node;

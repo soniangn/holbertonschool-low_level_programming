0f8ae7d5774d39dc05b2ff400790277cb6332f52
soniangn <nguyen.sonia@gmail.com>
2023-01-03 05:57:51 -0800

    fixed betty errors

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -12,22 +12,22 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    if (array == NULL)
-    {
-        return (-1);
-    }
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-    while (i < size)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-        {
-            return (i);
-        }
-        i++;
-    }
+	while (i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		i++;
+	}
 
-    return (-1);
+	return (-1);
 }
